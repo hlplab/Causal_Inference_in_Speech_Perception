5c9fc4770e7c84e05dcc015e34580814692f6beb
Shawn Cummings <scummin9@u.rochester.edu>
2023-06-08 15:29:38 +0200

    Add test data (all steps)

--- scripts/Acoustic-analyses.R
+++ scripts/Acoustic-analyses.R
@@ -1,15 +1,15 @@
 # Packages
 library(tidyverse)
 
-
 # Absolute filepath
 #setwd("../shawncummings/Documents/GitHub/")
 
-acoustics <- read_csv("./Causal_Inference_in_Speech_Perception/data/acoustics/fricative_cues_extracted_by_praat.csv")
+Exp <- read_csv("./Causal_Inference_in_Speech_Perception/data/acoustics/fricative_cues_extracted_by_praat.csv")
+Test <- read_csv("./Causal_Inference_in_Speech_Perception/data/acoustics/CISP_teststeps.csv.csv")
 
-CISP_CoG <- acoustics %>%
+CISP_Exp_CoG <- Exp %>%
   filter(study == "Liu-Jaeger-2018",
-         fricative %in% c("S", "SH", "?SSH")) %>%
+         fricative %in% c("S", "SH")) %>%
   select(word, fricative, shift_percent, cue_raw_M1) %>%
   mutate(bias = case_when(
     fricative == "S" & shift_percent == 0 ~ "SH",
@@ -24,11 +24,27 @@ Exposure_graph <- CISP_CoG %>%
   ggplot(aes(x = CoG,
              color = fricative)) +
   geom_density() +
-  facet_wrap(~bias)
+  facet_wrap(~bias) +
+  theme_bw()
 Exposure_graph
 
-CoG_measurements <- CISP_CoG %>%
+CISP_Test_CoG <- Test %>%
+  select(word, cue_raw_M1) %>%
+  rename(CoG = cue_raw_M1)
+  
+
+Test_graph <- CISP_Test_CoG %>%
+  ggplot(aes(x = as.numeric(word),
+             y = CoG,
+             label = word)) +
+  geom_label() +
+  theme_bw()
+Test_graph
+
+CoG_Exp_measurements <- CISP_CoG %>%
   group_by(fricative, shift_percent) %>%
   summarise(mean = mean(CoG),
             sd = sd(CoG))
 CoG_measurements
+
+CISP_Test_CoG
