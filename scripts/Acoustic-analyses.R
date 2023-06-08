479ec27ddb27552c23a335219e39d72257ba6c65
Shawn Cummings <scummin9@u.rochester.edu>
2023-06-08 15:44:38 +0200

    Comments

--- scripts/Acoustic-analyses.R
+++ scripts/Acoustic-analyses.R
@@ -4,9 +4,11 @@ library(tidyverse)
 # Absolute filepath
 #setwd("../shawncummings/Documents/GitHub/")
 
+# Raw data from praat scripts
 Exp <- read_csv("./Causal_Inference_in_Speech_Perception/data/acoustics/fricative_cues_extracted_by_praat.csv")
 Test <- read_csv("./Causal_Inference_in_Speech_Perception/data/acoustics/CISP_teststeps.csv.csv")
 
+# Format to just things we care about
 CISP_Exp_CoG <- Exp %>%
   filter(study == "Liu-Jaeger-2018",
          fricative %in% c("S", "SH")) %>%
@@ -15,10 +17,21 @@ CISP_Exp_CoG <- Exp %>%
     fricative == "S" & shift_percent == 0 ~ "SH",
     fricative == "S" & shift_percent == 50 ~ "S",
     fricative == "SH" & shift_percent == 0 ~ "S",
-    fricative == "SH" & shift_percent == 50 ~ "SH",
-    fricative == "?SSH" ~ "test")) %>%
+    fricative == "SH" & shift_percent == 50 ~ "SH")) %>%
   rename(CoG = cue_raw_M1)
 
+CISP_Test_CoG <- Test %>%
+  select(word, cue_raw_M1) %>%
+  rename(CoG = cue_raw_M1)
+
+CoG_Exp_measurements <- CISP_Exp_CoG %>%
+  group_by(fricative, shift_percent) %>%
+  summarise(mean = mean(CoG),
+            sd = sd(CoG))
+
+rm(Exp, Test)
+
+# Some visualizations for sanity checks
 Exposure_graph <- CISP_CoG %>%
   filter(fricative != "?SSH") %>%
   ggplot(aes(x = CoG,
@@ -28,11 +41,6 @@ Exposure_graph <- CISP_CoG %>%
   theme_bw()
 Exposure_graph
 
-CISP_Test_CoG <- Test %>%
-  select(word, cue_raw_M1) %>%
-  rename(CoG = cue_raw_M1)
-  
-
 Test_graph <- CISP_Test_CoG %>%
   ggplot(aes(x = as.numeric(word),
              y = CoG,
@@ -41,10 +49,13 @@ Test_graph <- CISP_Test_CoG %>%
   theme_bw()
 Test_graph
 
-CoG_Exp_measurements <- CISP_CoG %>%
-  group_by(fricative, shift_percent) %>%
-  summarise(mean = mean(CoG),
-            sd = sd(CoG))
-CoG_measurements
 
+## VALUES to use
+# CoG for all test steps (ASHI-high)
 CISP_Test_CoG
+
+# CoG for all exposure items
+CISP_Exp_CoG
+
+# Means and SDs for exposure item types
+CoG_Exp_measurements
